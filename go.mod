9d33c2e2f6b7701f88a4f2b9bf8325c0a95e65bd
Jean-Francois Smigielski <jf.smigielski@gmail.com>
2021-02-24 22:07:20 +0100

    Minor reorg, Update the User-Agent filter
    Based on anoymized http access log of https://www.hegemonie.be

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/jfsmig/nginx-logs
 go 1.14
 
 require (
+	github.com/jfsmig/nginx-logs v0.0.0-20201015114532-b92923f91faf // indirect
 	github.com/rs/zerolog v1.18.0
 	github.com/spf13/pflag v1.0.3
 )
--- nlogx/main.go
+++ nlogx/main.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2020 nlogx's AUTHORS
+// Copyright (C) 2020-2021 nlogx's AUTHORS
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -22,13 +22,18 @@ import (
 )
 
 const (
+	MinColumns     = 145
+	DefaultColumns = 200
+)
+
+const (
 	stepBegin   = iota
 	stepBare    = iota
 	stepQuote   = iota
 	stepBracket = iota
 )
 
-type record0 struct {
+type RawRecord struct {
 	ip       string
 	when     string
 	req      string
@@ -37,7 +42,7 @@ type record0 struct {
 	agent    string
 }
 
-type Record1 struct {
+type Record struct {
 	Ip   string `json:"src"`
 	When int64  `json:"t"`
 
@@ -55,40 +60,67 @@ var Logger = zerolog.
 	With().Timestamp().Logger()
 
 var avoidedAgents = []string{
-	"^curl",
-	"^Java",
-	"^Python",
-	"^Go",
-	"^Twingly",
-	"^Qwant",
-	"^Validator",
-	"^Screaming",
-	"bot", "Bot",
-	"crawler", "Crawler",
-	"screamingfrog",
+	"^Apache-HttpClient",
+	"Analyzer",
+	"Bing",
+	"Bot",
+	"Crawler",
+	"^Embarcadero",
+	"Go",
 	"Google Favicon",
-	"python-requests",
-	"phpunit",
-	"phpmyadmin",
+	"HeadlessChrome",
+	"IDBTE4M",
+	"^Java",
+	"Jigsaw",
+	"NetSystemsResearch",
+	"NetcraftSurveyAgent",
+	"^Nuclei",
+	"Python",
+	"Qwant",
+	"RestSharp",
+	"Scanner",
+	"^SMRF",
+	"Screaming",
+	"^Scrapy",
+	"Spider",
+	"^TBI-HttpOpenPlugi",
+	"Twingly",
+	"Validator",
+	"^W3C_Unicorn",
+	"^adreview",
+	"^axios",
+	"baidu",
+	"bot",
+	"^colly",
+	"cortex",
+	"crawler",
+	"curl",
+	"evc-batch",
+	"facebookextern",
+	"^http",
+	"^github-camo",
 	"jsonws",
+	"lighthouse",
+	"phpmyadmin",
+	"phpunit",
+	"python-requests",
 	"solr",
 	"spider",
-	"lighthouse",
-	"HeadlessChrome",
-	"BingPreview",
-	"NetcraftSurveyAgent",
+	"webtech",
+	"xpanse",
 }
 
-var avoidedAddresses = map[string]bool{
+var avoidedAddresses = []string{
 	// Whitelist
-	"127.0.0.1":      true, // localhost for testing purposes
-	"91.173.184.121": true, // jfs
-	"185.75.141.25":  true, // scalair, openio
-	"93.70.13.60":    true, // Ruben
+	"127.0.0.1",      // localhost for testing purposes
+	"91.173.184.121", // jfs adsl free
+	"92.158.80.82",   // jfs fibre orange
 	// Blacklist
 }
 
-var errMalformedQuery = errors.New("Invalid query")
+var avoidedReferrer = []string{
+	"51.38.234.78",
+}
 
 var versionToCode = map[string]int{
 	"HTTP/0.9": 0,
@@ -97,11 +129,12 @@ var versionToCode = map[string]int{
 	"HTTP/2.0": 2,
 }
 
-type StringPredicateFunc func(string) bool
-type TimePredicateFunc func(int64) bool
+var errMalformedQuery = errors.New("Invalid query")
 
-func expandRecords(src <-chan record0, dateKo TimePredicateFunc) <-chan Record1 {
-	out := make(chan Record1, 64)
+type SieveFilter func(r Record) bool
+
+func expandRecords(src <-chan RawRecord) <-chan Record {
+	out := make(chan Record, 64)
 	go func() {
 		defer close(out)
 		for r0 := range src {
@@ -120,10 +153,7 @@ func expandRecords(src <-chan record0, dateKo TimePredicateFunc) <-chan Record1 
 				Logger.Debug().Str("date", r0.when).Err(err).Msg("Invalid date")
 				continue
 			}
-			if dateKo(when) {
-				continue
-			}
-			out <- Record1{
+			out <- Record{
 				Ip:       r0.ip,
 				When:     when,
 				Method:   method,
@@ -138,8 +168,8 @@ func expandRecords(src <-chan record0, dateKo TimePredicateFunc) <-chan Record1 
 	return out
 }
 
-func parseRecords(src io.Reader, addrKo, agentKo StringPredicateFunc) <-chan record0 {
-	out := make(chan record0, 64)
+func parseRecords(src io.Reader) <-chan RawRecord {
+	out := make(chan RawRecord, 64)
 	go func() {
 		defer close(out)
 		in := bufio.NewReader(src)
@@ -153,13 +183,7 @@ func parseRecords(src io.Reader, addrKo, agentKo StringPredicateFunc) <-chan rec
 			}
 			ip := line[0]
 			agent := line[8]
-			if addrKo(ip) {
-				return
-			}
-			if agentKo(agent) {
-				return
-			}
-			out <- record0{
+			out <- RawRecord{
 				ip:       ip,
 				when:     line[3],
 				req:      line[4],
@@ -276,60 +300,111 @@ func fmtTime(epoch int64) string {
 	return time.Unix(epoch, 0).Format("2006-01-02 15:04:05")
 }
 
+func filter(in <-chan Record, ko func(Record) bool) <-chan Record {
+	out := make(chan Record, 32)
+	go func() {
+		defer close(out)
+		for r := range in {
+			if !ko(r) {
+				out <- r
+			}
+		}
+	}()
+	return out
+}
+
 func main() {
 	var flagVerbose bool
 	var flagFilterAgent, flagFilterSource bool
 	var flagJson, flagHuman bool
 	var filteredDays int
+	var nbColumns int64 = DefaultColumns
 	var thisAddr []string
 
 	zerolog.SetGlobalLevel(zerolog.InfoLevel)
 
+	strCols := os.Getenv("COLUMNS")
+	if strCols != "" {
+		var err error
+		nbColumns, err = strconv.ParseInt(strCols, 10, 32)
+		if err != nil {
+			Logger.Warn().Err(err).Msg("Invalid line length (env: COLUMNS)")
+			nbColumns = DefaultColumns
+		}
+	}
+	if nbColumns < MinColumns {
+		nbColumns = MinColumns
+	}
+
 	pflag.BoolVarP(&flagVerbose, "verbose", "v", false, "Increase the verbosity level")
 	pflag.BoolVarP(&flagHuman, "human", "H", false, "Display a human-readable output")
 	pflag.BoolVarP(&flagJson, "json", "j", false, "Dump JSON records at the output")
-	pflag.BoolVarP(&flagFilterAgent, "agent", "A", false, "Filter on User-Agent")
-	pflag.BoolVarP(&flagFilterSource, "source", "s", false, "Filter well-known sources")
+	pflag.BoolVarP(&flagFilterAgent, "agent", "A", false, "Hide suspicious User-Agent")
+	pflag.BoolVarP(&flagFilterSource, "source", "s", false, "Hide well-known sources")
 	pflag.IntVarP(&filteredDays, "days", "d", 0, "Restrict to a time window (in days)")
+	pflag.Int64VarP(&nbColumns, "columns", "c", nbColumns, "Max line length for the human-readable display")
 	pflag.StringSliceVarP(&thisAddr, "addr", "x", make([]string, 0), "Only display record from specific and explicit sources")
 	pflag.Parse()
 
-	addrSieve := func(addr string) bool { return false }
-	agentSieve := func(agent string) bool { return false }
-	dateSieve := func(epoch int64) bool { return false }
+	// By default, our 4 filters are just passthrough, they accept everything
+	addrSieve := func(Record) bool { return false }
+	agentSieve := func(Record) bool { return false }
+	dateSieve := func(Record) bool { return false }
+	referrerSieve := func(Record) bool { return false }
 
 	if flagFilterAgent {
 		expr, agentRegex, err := makeOrRegex(avoidedAgents)
 		if err != nil {
 			Logger.Fatal().Str("expr", expr).Err(err).Msg("Failed to build the rege matching the agents")
+		} else {
+			Logger.Info().Str("expr", expr).Msg("agents")
 		}
-		agentSieve = func(agent string) bool {
-			return agent == "-" || agentRegex.MatchString(agent)
-		}
+		agentSieve = func(r Record) bool { return r.Agent == "-" || agentRegex.MatchString(r.Agent) }
 	}
+
 	if len(thisAddr) > 0 {
-		addrSieve = func(addr string) bool {
+		addrSieve = func(r Record) bool {
 			for _, s := range thisAddr {
-				if s == addr {
+				if s == r.Ip {
 					return false
 				}
 			}
 			return true
 		}
 	} else if flagFilterSource {
-		addrSieve = func(addr string) bool {
-			return avoidedAddresses[addr]
-		}
-	}
-	if filteredDays > 0 {
-		oldest := time.Now().AddDate(0, 0, -filteredDays).Unix()
-		dateSieve = func(epoch int64) bool {
-			return epoch < oldest
-		}
+		addrSieve = func() SieveFilter {
+			mySet := make(map[string]bool)
+			for _, s := range avoidedAddresses {
+				mySet[s] = true
+			}
+			return func(r Record) bool { return mySet[r.Ip] }
+		}()
 	}
 
-	r0 := parseRecords(os.Stdin, addrSieve, agentSieve)
-	r1 := expandRecords(r0, dateSieve)
+	if filteredDays > 0 {
+		oldest := time.Now().AddDate(0, 0, -filteredDays).Unix()
+		dateSieve = func(r Record) bool { return r.When < oldest }
+	}
+
+	if len(avoidedReferrer) > 0 {
+		expr, refRegex, err := makeOrRegex(avoidedReferrer)
+		if err != nil {
+			Logger.Fatal().Str("expr", expr).Err(err).Msg("Failed to build the regex matching the referrers")
+		}
+		referrerSieve = func(r Record) bool { return refRegex.MatchString(r.Referrer) }
+	}
+
+	// Create a source of information
+	r0 := parseRecords(os.Stdin)
+
+	// Pack a pipeline of filters to trim unwanted records
+	r1 := expandRecords(r0)
+	r1 = filter(r1, dateSieve)
+	r1 = filter(r1, addrSieve)
+	r1 = filter(r1, agentSieve)
+	r1 = filter(r1, referrerSieve)
+
+	// Dump the expected output
 	if flagJson {
 		encoder := json.NewEncoder(os.Stdout)
 		for r := range r1 {
@@ -337,14 +412,13 @@ func main() {
 		}
 	} else {
 		if flagHuman {
-			const cols = 200
-			format := fmt.Sprintf("%%-15s %%-19s %%-3d %%-60.60s  %%-40.40s  %%.%ds\n", cols-145)
+			format := fmt.Sprintf("%s %%-15s %%-3d %%-60.60s  %%-40.40s  %%.%ds\n", nbColumns-145)
 			for r := range r1 {
-				fmt.Printf(format, r.Ip, fmtTime(r.When), r.Code, r.Path, r.Referrer, r.Agent)
+				fmt.Printf(format, fmtTime(r.When), r.Ip, r.Code, r.Path, r.Referrer, r.Agent)
 			}
 		} else {
 			for r := range r1 {
-				fmt.Printf("%s %s %d %s %s %q\n", r.Ip, fmtTime(r.When), r.Code, r.Path, r.Referrer, r.Agent)
+				fmt.Printf("%s %-15s %d %s %s %q\n", fmtTime(r.When), r.Ip, r.Code, r.Path, r.Referrer, r.Agent)
 			}
 		}
 	}
